51105719bc7981e93dd6c75a38e3892597ec6f0a
OasisDefinitelyMaybe <kbh26499@gmail.com>
2023-12-27 19:53:52 +0900

    JPA 및 레포지토리 설계 및 쿼리메서드

---
+++ 5.스프링부트/exam02/src/main/java/org/choongang/entities/BoardData.java
@@ -0,0 +1,30 @@
+package org.choongang.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+public class BoardData extends Base {
+    @Id
+    @GeneratedValue
+    private Long seq;
+
+    @Column(length = 100, nullable = false)
+    public String subject;
+
+    @Lob
+    @Column(nullable = false)
+    public String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userNo")
+    private Member member;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    private List<HashTag> tags = new ArrayList<>();
+
+}
